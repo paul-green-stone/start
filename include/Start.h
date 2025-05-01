5ca0163d556380f2b7d0ae7ad62b37557ab24669
paulstone <stone.paul.green>
2025-05-01 23:01:01 +0300

    Add a header file that contains most of the systems

---
+++ include/Start.h
@@ -0,0 +1,15 @@
+#ifndef _START_HEADER_H
+#define _START_HEADER_H
+
+/* ================================================================ */
+
+#include "Window.h"
+#include "Application.h"
+#include "Clock.h"
+#include "Input.h"
+#include "Texture.h"
+#include "Text.h"
+
+/* ================================================================ */
+
+#endif /* _START_HEADER_H */
