e2abc65be3c46ad575c72b0e7b545936f73978e9
paulstone <stone.paul.green>
2025-05-04 23:28:39 +0300

    Add a simple data storage for in-game resources

---
+++ include/Manager.h
@@ -0,0 +1,43 @@
+#ifndef _START_RESOURCE_MANAGER_H
+#define _START_RESOURCE_MANAGER_H
+
+/* ================================================================ */
+
+/**
+ * Inserts a key-data pair into the storage.
+ * 
+ * @param key A null-terminated string representing the key to insert
+ * @param data A pointer to the data associated with the key to store in the hash table
+ * 
+ * @return Returns 0 if inserting the element is successful, 1 if the element is already in the storage, or –1 otherwise.
+ */
+int Manager_insert(const char* key, const void* data);
+
+/**
+ * Removes the key-data pair associated with the given `key` from the storage.
+ * 
+ * @param key A null-terminated string representing the key to remove
+ * 
+ * @return Returns pointer to the data associated with the removed key if found and removed. NULL if the key was not found in the table.
+ */
+void* Manager_remove(const char* key);
+
+/**
+ * Searches for a data element associated with the given string `key`.
+ * 
+ * @param key A null-terminated string representing the key to search for in the table
+ * 
+ * @return Returns a pointer to the data associated with the key if found. Returns NULL if the key does not exist in the table.
+ */
+void* Manager_lookup(const char* key);
+
+/**
+ * Returns the current number of elements in the storage.
+ * 
+ * @return The count of key-data pairs currently stored in the storage.
+ */
+size_t Manager_size(void);
+
+/* ================================================================ */
+
+#endif /* _START_RESOURCE_MANAGER_H */
---
+++ source/manager.c
@@ -0,0 +1,214 @@
+#include "../include/Manager.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+#define TABLE_SIZE 256
+#define CONSTANT 0.6180339887l
+
+#define _hash(key, i) (pjw_hash((key)) + ((i) * multiplicative_hash((key))))
+
+/* ================================================================ */
+
+struct resource_manager {
+
+    void* elements[TABLE_SIZE];             /* The array in which the elements are stored */
+    char* keys[TABLE_SIZE];                 /* The array of keys of the elements stored in the table  */
+
+    size_t size;                            /* The number of elements currently in the table */
+};
+
+static struct resource_manager Manager;
+
+/* ================================================================ */
+
+/* @param prime_size the size of the hash table, typically a prime number */
+
+/**
+ * Computes a hash value for a given string `key` using the Peter J. Weinberger (PJW) hash algorithm.
+ * 
+ * @param key a pointer to a null-terminated string to be hashed.
+ * 
+ * @return The computed hash value, an integer in the range `[0, TABLE_SIZE - 1]`
+ */
+static size_t pjw_hash(const char* key) {
+
+    size_t hash_coding;
+    size_t tmp;
+
+    hash_coding = 0;
+
+    /* ================================================================ */
+    /* == Hash the key by performing a number of bit operations on it = */
+    /* ================================================================ */
+
+    while (*key != '\0') {
+
+        hash_coding = (hash_coding << 4) + (*key);
+
+        if ((tmp = (hash_coding & 0xf0000000))) {
+
+            hash_coding ^= (tmp >> 24);
+            hash_coding ^= tmp;
+        }
+
+        key++;
+    }
+
+    /* ======== */
+
+    return hash_coding;
+}
+
+/* ================================================================ */
+
+/**
+ * Computes a hash value for a given null-terminated string key using a multiplicative hashing technique.
+ * 
+ * @param key a pointer to a null-terminated string to be hashed
+ * 
+ * @return Returns the computed hash value, an integer in the range [0, TABLE_SIZE - 1]
+ */
+static size_t multiplicative_hash(const char* key) {
+
+    size_t hash_coding;
+    size_t tmp;
+
+    hash_coding = 0;
+    tmp = 0;
+
+    while (*key != '\0') {
+        tmp = tmp * 31 + (size_t) *key;
+        key++;
+    }
+
+    hash_coding = floor(TABLE_SIZE * (fmod(tmp * CONSTANT, 1)));
+
+    /* ======== */
+
+    return hash_coding;
+}
+
+/* ================================================================ */
+
+void* Manager_lookup(const char* key) {
+
+    size_t position;
+    size_t i;
+
+    /* ======== */
+
+    for (i = 0; i < TABLE_SIZE; i++) {
+
+        position = _hash(key, i) % TABLE_SIZE;
+
+        if (Manager.elements[position] == NULL) {
+
+            /* Data was not found */
+            return NULL;
+        }
+        else if (strcmp(Manager.keys[position], key) == 0) {
+
+            /* Pass back the data from the table */
+            return Manager.elements[position];
+        }
+    }
+
+    /* ======== */
+
+    /* Data was not found */
+    return NULL;
+}
+
+/* ================================================================ */
+
+int Manager_insert(const char* key, const void* data) {
+
+    size_t position;
+    size_t i;
+
+    /* ======== */
+
+    /* Do not exceed the number of positions in the table */
+    if (Manager.size == TABLE_SIZE) {
+        return -1;
+    }
+
+    /* Do nothing if the data is already in the table */
+    if (Manager_lookup(key) != NULL) {
+        return 1;
+    }
+
+    for (i = 0; i < TABLE_SIZE; i++) {
+
+        position = _hash(key, i) % TABLE_SIZE;
+
+        if (Manager.elements[position] == NULL) {
+
+            printf("%d\n", position);
+
+            /*  Insert the data into the table */
+            Manager.elements[position] = (void*) data;
+            Manager.keys[position] = (char*) key;
+
+            Manager.size++;
+
+            /* ======== */
+
+            return 0;
+        }
+    }
+
+    /* ======== */
+
+    return -1;
+}
+
+/* ================================================================ */
+
+void* Manager_remove(const char* key) {
+
+    size_t position;
+    size_t i;
+
+    void* data;
+
+    /* ======== */
+
+    data = NULL;
+
+    for (i = 0; i < TABLE_SIZE; i++) {
+
+        position = _hash(key, i) % TABLE_SIZE;
+
+        if (Manager.elements[position] == NULL) {
+
+            /* Return that the data was not found */
+            break ;
+        }
+
+        else if (strcmp(Manager.keys[position], key) == 0) {
+
+            data = Manager.elements[position];
+
+            Manager.elements[position] = (Manager.keys[position] = NULL);
+            Manager.size--;
+
+            break ;
+        }
+    }
+
+    /* ======== */
+
+    return data;
+}
+
+/* ================================================================ */
+
+size_t Manager_size(void) {
+    return Manager.size;
+}
+
+/* ================================================================ */
