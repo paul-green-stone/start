bed3bd037896915d8bd02f1c4fac4070e40480c7
paulstone <stone.paul.green>
2025-05-05 20:25:14 +0300

    Add examples on file reading

---
+++ examples/file/main.c
@@ -0,0 +1,50 @@
+#include "../../include/Start.h"
+
+/* ================================================================ */
+
+int main(int argc, char** argv) {
+
+    config_t config;
+    config_setting_t* setting;
+    char* version;
+    int w, h;
+    double price;
+
+    int status = Conf_parse_file(&config, "example.conf");
+
+    if (status != 0) {
+
+        printf("Bad things happened\n");
+
+        /* ======== */
+
+        return -1;        
+    }
+
+    if (Conf_extract(&config, "version", STRING, &version) == 0) {
+        printf("Version: %s\n", version);
+    }
+
+    if (Conf_extract(&config, "application.window.size.w", INT, &w) == 0) {
+        printf("Width: %d\n", w);
+    }
+    if (Conf_extract(&config, "application.window.size.h", INT, &h) == 0) {
+        printf("Height: %d\n", h);
+    }
+
+    if (Conf_extract(&config, "application.window.title", STRING, &version) == 0) {
+        printf("Title: %s\n", version);
+    }
+
+    if (Conf_extract(&config, "application.books.[0].price", FLOAT, &price) == 0) {
+        printf("Price: %.2f\n", price);
+    }
+
+    config_destroy(&config);
+
+    /* ======== */
+
+    return 0;
+}
+
+/* ================================================================ */
