be22413646cc3faf8e128e719fbf70a23961b6d7
paulstone <stone.paul.green>
2025-05-08 23:25:16 +0300

    Add examples on using a new version of initialization

---
+++ examples/file_init/main.c
@@ -0,0 +1,45 @@
+#include "../../include/Start.h"
+
+/* ================================================================ */
+
+int main(int argc, char** argv) {
+
+    Start();
+    App_init();
+
+    /* ================ */
+
+    SDL_Event event;
+    SDL_Renderer* context = get_context();
+
+    while(App_isRunning()) {
+
+        while(SDL_PollEvent(&event)) {
+
+            switch (event.type) {
+
+                case SDL_QUIT:
+                    App_stop();
+                    break ;
+            }
+        }
+
+        /* ================================ */
+
+        SDL_SetRenderDrawColor(context, 255, 255, 255, 255);
+        SDL_RenderClear(context);
+
+        App_render();
+    }
+
+    /* ================ */
+
+    App_quit();
+    Stop();
+
+    /* ========= */
+
+    return 0;
+}
+
+/* ================================================================ */
