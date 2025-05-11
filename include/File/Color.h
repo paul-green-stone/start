6b00c63475fcbd49c1b68eb776c69aebb516ffd7
paulstone <stone.paul.green>
2025-05-12 01:33:28 +0300

    Add a header file that contains escepe sequence for color output

---
+++ include/File/Color.h
@@ -0,0 +1,34 @@
+#ifndef _START_COLOR_H
+#define _START_COLOR_H
+
+/* ================================================================ */
+/* ========================= DEFINITIONS ========================== */
+/* ================================================================ */
+
+/* Text Colors */
+#define RED "\x1b[31m"
+#define GREEN "\x1b[32m"
+#define BLUE "\x1b[34m"
+#define BLACK "\x1b[30m"
+#define YELLOW "\x1b[33m"
+#define MAGENTA "\x1b[35m"
+#define CYAN "\x1b[36m"
+#define WHITE "\x1b[37m"
+
+#define RESET "\x1b[0m"
+
+/* ================ */
+
+/* Background Colors */
+#define BRED "\x1b[41m"
+#define BGREEN "\x1b[42m"
+#define BBLUE "\x1b[44m"
+#define BBLACK "\x1b[40m"
+#define BYELLOW "\x1b[43m"
+#define BMAGENTA "\x1b[45m"
+#define BCYAN "\x1b[46m"
+#define BWHITE "\x1b[47m"
+
+/* ================================================================ */
+
+#endif /* _START_COLOR_H */
