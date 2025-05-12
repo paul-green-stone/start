8152fa59e2ce2a2ff7fcf1092e915bf6681943fa
paulstone <stone.paul.green>
2025-05-12 20:25:13 +0300

    Update example

--- examples/error/main.c
+++ examples/error/main.c
@@ -12,44 +12,51 @@ int main(int argc, char** argv) {
     int name;
     /* ======== */
 
-    config_init(&config);
+    // config_init(&config);
 
-    if ((status = Conf_parse_file(&config, filename)) != SSUCCESS) {
+    // if ((status = Conf_parse_file(&config, filename)) != SSUCCESS) {
 
-        config_destroy(&config);
-        error(stderr, "%s\n", Error_get_msg());
+    //     config_destroy(&config);
+    //     error(stderr, "%s\n", Error_get_msg());
 
-        /* ======== */
-        return status;
+    //     /* ======== */
+    //     return status;
+    // }
+
+    // if (Conf_extract(&config, "name", FLOAT, &name) == SSUCCESS) {
+    //     printf("Name = %d\n", name);
+    // }
+    // else {
+    //     error(stderr, "%s\n", Error_get_msg());
+    // }
+
+    // if (Conf_lookup(&config, "what", &setting) == SERR_ITEM_NOT_FOUND) {
+    //     error(stderr, "%s\n", Error_get_msg());
+    // }
+
+    // if (file_exists(NULL)) {
+    //     printf("Exists!\n");
+    // }
+
+    // status = directory_new("us");
+    // if (status == 1) {
+    //     printf("directory exists\n");
+    // }
+    // else if (status == SSUCCESS) {
+    //     printf("directory created\n");
+    // }
+    // else {
+    //     error(stderr, "%s\n", Error_get_msg());
+    // }
+    
+
+    if (Start() != SSUCCESS) {
+        error(stderr, "%s\n", Error_get_msg(), "");
     }
 
-    if (Conf_extract(&config, "name", FLOAT, &name) == SSUCCESS) {
-        printf("Name = %d\n", name);
-    }
-    else {
-        error(stderr, "%s\n", Error_get_msg());
-    }
+    Stop();
 
-    if (Conf_lookup(&config, "what", &setting) == SERR_ITEM_NOT_FOUND) {
-        error(stderr, "%s\n", Error_get_msg());
-    }
-
-    if (file_exists(NULL)) {
-        printf("Exists!\n");
-    }
-
-    status = directory_new("a");
-    if (status == 1) {
-        printf("directory exists\n");
-    }
-    else if (status == SSUCCESS) {
-        printf("directory created\n");
-    }
-    else {
-        error(stderr, "%s\n", Error_get_msg());
-    }
-
-    config_destroy(&config);
+    //config_destroy(&config);
 
     /* ======== */
     return EXIT_SUCCESS;
