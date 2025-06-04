09fc382209af396cd088b4ac2ad236ce14833c34
paulstone <stone.paul.green>
2025-06-05 00:00:36 +0300

    .

---
+++ include/Widget/Widgets.h
@@ -0,0 +1,26 @@
+#ifndef _START_WIDGETS_COLLECTION_H
+#define _START_WIDGETS_COLLECTION_H
+
+/* ================================================================ */
+
+/**
+ * Widget base class
+ */
+extern const void* Widget;
+
+/**
+ * Usage: `Widget_create(Button, x, y, font, color, label, texture);`
+ * 
+ * @param Button pointer to the button widget descriptor
+ * @param x x-coordinate of the button's position on the screen
+ * @param y y-coordinate of the button's position on the screen
+ * @param font pointer to an `SDL_ttf` font used to render the button's label text (`TTF_Font*`)
+ * @param color pointer to an `SDL_Color` structure specifying the color of the label text (`SDL_Color*`)
+ * @param label null-terminated string containing the text displayed on the button (`const char*`)
+ * @param texture optional pointer to a custom texture object representing an image to be rendered as the button's background instead of the default label
+ */
+extern const void* Button;
+
+/* ================================================================ */
+
+#endif /* _START_WIDGETS_COLLECTION_H */
