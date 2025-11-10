17ea0c09f1667198a8ebe793f78884835bf331e4
paulstone <stone.paul.green>
2025-11-10 03:07:48 +0300

    Update a header file

--- include/Start.h
+++ include/Start.h
@@ -7,31 +7,26 @@
 extern "C" {
 #endif
 
-#include "Core.h"
-#include "Window.h"
-#include "Application.h"
-#include "Clock.h"
-#include "Input.h"
-#include "Texture.h"
-#include "Text.h"
-#include "Manager.h"
-#ifndef __EMSCRIPTEN__
-#include "File/conf.h"
-#endif
 #include "File/Color.h"
-#include "State/State.h"
-#include "Math/Core.h"
-#include "Math/Vector2D.h"
-#include "Widget/Menu.h"
-#include "Widget/Widgets.h"
-#include "Widget/Widget.h"
-#include "Error.h"
-#include "Camera.h"
+#include "File/conf.h"
 #include "Animation.h"
-
+#include "Application.h"
+#include "Camera.h"
+#include "Clock.h"
+#include "Core.h"
+#include "Error.h"
+#include "Input.h"
+#include "Manager.h"
+#include "Text.h"
+#include "Texture.h"
+#include "Window.h"
+#include "State/State.h"
+#include "Widget/Menu.h"
+#include "Widget/Widget.h"
+#include "Widget/Widgets.h"
 #include "Widget/Button/Button.h"
-#include "Widget/TextInput/TextInput.h"
 #include "Widget/SelectWidget/SelectWidget.h"
+#include "Widget/TextInput/TextInput.h"
 
 /* ================================================================ */
 
